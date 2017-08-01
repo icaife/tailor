9d158bd8785e808068c2fc3c05f6721b5496e288
leon <806686981@qq.com>
2017-08-02 00:45:15 +0800

    update middleware

--- server/server.js
+++ server/server.js
@@ -1,123 +1,76 @@
 "use strict";
 
-// const
-// 	Path = require("path"),
-// 	Webpack = require("webpack"),
-// 	BrowserSync = require("browser-sync"),
-// 	Vinyl = require("vinyl-fs"),
-// 	Constant = require("../constant"),
-// 	bs = BrowserSync.create(),
-// 	basic = Constant.basic,
-// 	server = Constant.server;
-
-// let queue = [];
-
-// function reload(event, file) {
-// 	queue.push(file);
-
-// 	setTimeout(function() {
-// 		file = queue[0];
-
-// 		if (/\.css$/.test(file)) {
-// 			Vinyl
-// 				.src(file)
-// 				.pipe(bs.stream({
-// 					once: false
-// 				}));
-// 		} else {
-// 			bs.reload();
-// 		}
-// 		queue.length = 0;
-// 	}, 1000);
-// }
-
-// module.exports = {
-// 	run: function(callback) {
-// 		return new Promise((resolve, reject) => {
-// 			bs.init({
-// 				proxy: basic.domain,
-// 				port: server.port,
-// 				open: false,
-// 				relaodDelay: 200,
-// 				reloadDebounce: 1000
-// 			}, function(e) {
-// 				resolve(e);
-// 				// bs.watch(Path.join(basic.root, basic.dest, "**/*.{php,html,js,css}"), reload);
-// 			});
-// 		});
-// 	}
-// };
-
-
-// const
-// 	Webpack = require("webpack"),
-// 	WebpackDevServer = require("webpack-dev-server"),
-// 	webpackConfig = require("./webpack.config.js"),
-// 	compiler = Webpack(webpackConfig),
-// 	config = require("./constant").server,
-// 	server = new WebpackDevServer(compiler, config.devServer);
-
-// server.listen(8080);
-
 const
-	Webpack = require("webpack"),
-	DevMiddleware = require("webpack-dev-middleware"),
-	HotMiddleware = require("webpack-hot-middleware"),
-	Config = require("../config"),
-	basic = Config.basic,
-	webpackConfig = Config.webpack,
-	serverConfig = Config.server;
+    Webpack = require("webpack"),
+    Koa = require("koa"),
+    KoaMiddleware = require("koa-webpack-middleware"),
+    DevMiddleware = KoaMiddleware.devMiddleware, //require("webpack-dev-middleware"),
+    HotMiddleware = KoaMiddleware.hotMiddleware, //require("webpack-hot-middleware"),
+    Config = require("../config"),
+    basic = Config.basic,
+    webpackConfig = Config.webpack,
+    serverConfig = Config.server,
+    router = new(require("koa-router"))();
 
-function redirectMiddle(req, res, next) {
-	let reqUrl = req.url;
+function redirectMiddleware(ctx, next) {
+    let req = ctx.req;
+    let res = ctx.res;
+    let reqUrl = req.url;
 
-	if (!/^\/?assets/.test(reqUrl)) {}
+    if (!/^\/?assets/.test(reqUrl)) {}
 
-	next();
+    console.log(reqUrl);
+    return next();
 }
 
 function run() {
-	const
-		compiler = Webpack(webpackConfig),
-		Http = require("http"),
-		Express = require("express"),
-		devMiddleware = DevMiddleware(compiler, serverConfig.devServer),
-		hotMiddleware = HotMiddleware(compiler),
-		app = new Express();
+    const
+        compiler = Webpack(webpackConfig),
+        devMiddleware = DevMiddleware(compiler, serverConfig.devServer),
+        hotMiddleware = HotMiddleware(compiler),
+        app = new Koa();
 
-	compiler.plugin("compilation", function(compilation) {
-		// compilation.plugin("html-webpack-plugin-after-emit", function(data, cb) {
-		// 	console.log("[HMR] html changed,refresh page..");
-		// 	hotMiddleware.publish({
-		// 		action: "reload"
-		// 	});
-		// 	cb();
-		// });
+    compiler.plugin("compilation", function(compilation) {
+        // compilation.plugin("html-webpack-plugin-after-emit", function(data, cb) {
+        // 	console.log("[HMR] html changed,refresh page..");
+        // 	hotMiddleware.publish({
+        // 		action: "reload"
+        // 	});
+        // 	cb();
+        // });
 
-		compilation.plugin("succeed-module", function(module) {
-			let resource = module.resource;
+        compilation.plugin("succeed-module", function(module) {
+            let resource = module.resource;
+            let reg = new RegExp(`.(${config.basic.html.ext.join("|")})$`.replace(/\./g, "\\."), "i");
 
-			if (/(html|php)$/.test(resource)) {
-				hotMiddleware.publish({
-					action: "reload",
-					src: resource
-				});
-			}
-		});
-	});
+            if (reg.test(resource)) {
+                hotMiddleware.publish({
+                    action: "reload",
+                    src: resource
+                });
+            }
+        });
+    });
 
-	app
-		.set("trust proxy", "loopback")
-		.use(devMiddleware)
-		.use(hotMiddleware)
-		.use(redirectMiddle)
-		.listen(serverConfig.port, serverConfig.host, function(err) {
-			if (err) {
-				throw new Error(err);
-			}
-		});
+    router.get("/:name", function(ctx, next) {
+        ctx.body = "Hello " + ctx.params.name;
+
+        return next();
+    });
+
+    app
+        .use(devMiddleware)
+        .use(hotMiddleware)
+        // .use(redirectMiddleware)
+        .use(router.routes())
+        .use(router.allowedMethods())
+        .listen(serverConfig.port, serverConfig.host, function(err) {
+            if (err) {
+                throw new Error(err);
+            }
+        });
 }
 
 module.exports = {
-	run: run
+    run: run
 };
