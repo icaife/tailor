fed42dc32b56f654822cb2218d6c30e1935592aa
System Administrator <root@LeondeMacBook-Pro.local>
2017-08-04 00:06:18 +0800

    update

--- index.js
+++ index.js
@@ -5,18 +5,23 @@
 "use strict";
 
 const
-	Path = require("path"),
-	Shell = require("shelljs"),
-	Log = require("./lib/util/log"),
-	Webpack = require("webpack"),
-	Constant = require("./constant"),
-	Config = require("./config"),
-	Serve = require("./server");
+    Path = require("path"),
+    Shell = require("shelljs"),
+    Log = require("./lib/util/log"),
+    Webpack = require("webpack"),
+    Constant = require("./constant"),
+    Config = require("./config"),
+    Serve = require("./server");
 
-Log.info("work path-> " + Path.join(Config.basic.root));
+Log.info(`\nwork info:\n\tpath:${Log.chalk.blue(Path.join(Config.basic.root))}\n\tenv:${Log.chalk.blue(Config.basic.env)}`);
+let destPath = Path.join(Config.basic.root, Config.basic.dest);
+Shell.rm("-rf", destPath)
+Log.info("\nclean " + Log.chalk.blue(destPath) + " done");
+
+console.log(Shell.exec("git grep -n -P module.exports")[0]);
 
 if (Config.basic.env === Constant.env.development) {
-	Serve.run();
+    Serve.run();
 } else {
-	Shell.exec("webpack --colors --config webpack.config.js");
+    Shell.exec("webpack --colors --config webpack.config.js");
 }
