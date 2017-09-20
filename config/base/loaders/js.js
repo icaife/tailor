29b5d65d46df06390a0dbc333a9df18f77b0611d
Leon Cai <leon.cai@toursforfun.com>
2017-09-20 21:22:59 +0800

    babel-loader preset env

--- config/base/loaders/js.js
+++ config/base/loaders/js.js
@@ -11,13 +11,18 @@ module.exports = (config) => {
             loader: "babel-loader",
             options: {
                 presets: [
-                    Path.join(tailorConfig.path, "./node_modules/babel-preset-es2015"),
-                    Path.join(tailorConfig.path, "./node_modules/babel-preset-stage-2")
+                    "env", {
+                        targets: {
+                            browsers: ["last 2 version", "ie 9"]
+                        }
+                    }
+                    // Path.join(tailorConfig.path, "./node_modules/babel-preset-env"),
+                    // Path.join(tailorConfig.path, "./node_modules/babel-preset-stage-2")
                 ],
                 babelrc: false,
                 retainLines: true,
                 cacheDirectory: true,
-                // exclude: /node_modules|vendor/
+                exclude: /node_modules|vendor/
             }
         },
         eslintLoader = {
