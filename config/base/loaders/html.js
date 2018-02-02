2a3878b09b37d04851d5dc9e3f30100705d672e4
Leon Cai <leon.cai@toursforfun.com>
2018-02-02 18:34:01 +0800

    update

--- config/base/loaders/html.js
+++ config/base/loaders/html.js
@@ -3,64 +3,68 @@
  * @author Leon.Cai
  */
 
-const
-    Path = require("path");
+const Path = require("path");
 
-module.exports = (config) => {
-    let
-        outputConfig = config.output,
-        inputConfig = config.input;
+module.exports = config => {
+	let outputConfig = config.output,
+		inputConfig = config.input;
 
-    let
-        htmlLoader = {
-            loader: "html-loader",
-            options: {}
-        },
-        /**
-         * @see https://aui.github.io/art-template/zh-cn/docs/rules.html
-         */
-        artTemplateLoader = {
-            loader: "art-template-loader",
-            options: {
-                extname: "." + inputConfig.html.ext[0],
-                cache: !true,
-                htmlResourceRoot: Path.join(config.root, inputConfig.path),
-                root: Path.join(config.root, inputConfig.path),
-                htmlResourceRules: [
-                    /<(?:img)[^>]+\b(?:\w+-?)?(?:src)="([^"{}]*(?:jpg|png|gif|webp|bmp))"[^>]*?>/img, //img tag
-                ],
-                rules: [{
-                        test: /{{raw}}([\w\W]*?){{\/raw}}/,
-                        use: function(match, code) {
-                            return {
-                                output: 'raw',
-                                code: JSON.stringify(code)
-                            }
-                        }
-                    }, {
-                        test: /@{{([@#]?)[ \t]*([\w\W]*?)[ \t]*}}/g, //TODO:vue or other javascript,php blade template
-                        use: function(match, raw, close, code) {
-                            return {
-                                code: `"${match.toString()}"`,
-                                output: "raw"
-                            };
-                        }
-                    }, {
-                        test: /{#([@#]?)[ \t]*([\w\W]*?)[ \t]*#}/g, //TODO:php blade template
-                        use: function(match, raw, close, code) {
-                            return {
-                                code: `"${match.toString()}"`.replace(/\.(\w+)/g, '[\'$1\']').replace(/{#/g, "{{").replace(/#}/g, "}}"),
-                                output: "raw"
-                            };
-                        }
-                    },
-                    ...require("art-template").defaults.rules,
-                ],
-            }
-        };
+	let htmlLoader = {
+			loader: "html-loader",
+			options: {}
+		},
+		/**
+		 * @see https://aui.github.io/art-template/zh-cn/docs/rules.html
+		 */
+		artTemplateLoader = {
+			loader: "art-template-loader",
+			options: {
+				debug: config.env === "dev",
+				extname: "." + inputConfig.html.ext[0],
+				cache: !true,
+				htmlResourceRoot: Path.join(config.root, inputConfig.path),
+				root: Path.join(config.root, inputConfig.path),
+				htmlResourceRules: [
+					/<(?:img)[^>]+\b(?:\w+-?)?(?:src)="([^"{}]*(?:jpg|png|gif|webp|bmp))"[^>]*?>/gim //img tag
+				],
+				rules: [
+					{
+						test: /{{raw}}([\w\W]*?){{\/raw}}/,
+						use: function(match, code) {
+							return {
+								output: "raw",
+								code: JSON.stringify(code)
+							};
+						}
+					},
+					{
+						test: /@{{([@#]?)[ \t]*([\w\W]*?)[ \t]*}}/g, //TODO:vue or other javascript,php blade template
+						use: function(match, raw, close, code) {
+							return {
+								code: `"${match.toString()}"`,
+								output: "raw"
+							};
+						}
+					},
+					{
+						test: /{#([@#]?)[ \t]*([\w\W]*?)[ \t]*#}/g, //TODO:php blade template
+						use: function(match, raw, close, code) {
+							return {
+								code: `"${match.toString()}"`
+									.replace(/\.(\w+)/g, "['$1']")
+									.replace(/{#/g, "{{")
+									.replace(/#}/g, "}}"),
+								output: "raw"
+							};
+						}
+					},
+					...require("art-template").defaults.rules
+				]
+			}
+		};
 
-    return {
-        htmlLoader,
-        artTemplateLoader
-    };
+	return {
+		htmlLoader,
+		artTemplateLoader
+	};
 };
